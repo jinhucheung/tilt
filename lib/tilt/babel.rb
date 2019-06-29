b12ef9ed7b235d7c379707f7dc7b4625c437e9ca
jim <hi.jinhu.zhang@gmail.com>
2019-06-30 00:57:52 +0800

    Create babel.rb

---
+++ lib/tilt/babel.rb
@@ -0,0 +1,13 @@
+module Tilt
+  class BabelTemplate < Template
+    self.default_mime_type = 'application/javascript'
+
+    def prepare
+      options[:filename] ||= file
+    end
+
+    def evaluate(scope, locals, &block)
+      @output ||= Babel::Transpiler.transform(data)["code"]
+    end
+  end
+end
